bd8ed97c1799f586214aa48e3f35b1874320ecf5
Joshua Juanchini <joshua.juanchini@gmail.com>
2019-12-15 20:30:48 -0300

    First root request (hello world)

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,15 +4,17 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/signup', userController.signup);
-
 router.post('/login', userController.login);
 
 router.get('/user/:userId', userController.allowIfLoggedin, userController.getUser);
-
 router.get('/users', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), userController.getUsers);
 
 router.put('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
 
 router.delete('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
+
+router.get('/', (req, res) => {
+    res.send('Hello world')
+});
 
 module.exports = router;
