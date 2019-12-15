8996bb9a55a57b32efca354104bf8fa243f0d1a6
Joshua Juanchini <joshua.juanchini@gmail.com>
2019-12-15 17:48:04 -0300

    re-identation

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,23 +3,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
- email: {
-  type: String,
-  required: true,
-  trim: true
- },
- password: {
-  type: String,
-  required: true
- },
- role: {
-  type: String,
-  default: 'basic',
-  enum: ["basic", "supervisor", "admin"]
- },
- accessToken: {
-  type: String
- }
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'basic',
+        enum: ["basic", "supervisor", "admin"]
+    },
+    accessToken: {
+        type: String
+    }
 });
 
 const User = mongoose.model('user', UserSchema);
