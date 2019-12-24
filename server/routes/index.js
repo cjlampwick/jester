6a4ebd25971f330e4fa64319d2dff419a4572c1c
cjlanwick <joshua.juanchini@gmail.com>
2019-12-24 15:31:22 -0300

    comment groups router and use index router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,55 +1,67 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const groupController = require('../controllers/groupController');
+
+//this will be here until we create all the controllers
+let data = {
+  actions: []
+};
 
 /* GET home page. */
 router.get('/', userController.allowIfLoggedin, function (req, res, next) {
   res.render('index', {
     title: 'Home',
-    view: 'dashboard'
+    view: 'dashboard',
+    data
   });
 });
 
 router.get('/config', userController.allowIfLoggedin, function (req, res, next) {
   res.render('index', {
     title: 'Configuration',
-    view: 'config'
+    view: 'config',
+    data
   });
 });
 
 router.get('/messages', userController.allowIfLoggedin, function (req, res, next) {
   res.render('index', {
     title: 'Messages',
-    view: 'messages'
+    view: 'messages',
+    data
   });
 });
 
 router.get('/dashboard', userController.allowIfLoggedin, function (req, res, next) {
   res.render('index', {
     title: 'Dashboard',
-    view: 'dashboard'
+    view: 'dashboard',
+    data
   });
 });
 
 router.get('/agents', userController.allowIfLoggedin, function (req, res, next) {
   res.render('index', {
     title: 'Agents',
-    view: 'agents'
+    view: 'agents',
+    data
   });
 });
 
 router.get('/customers', userController.allowIfLoggedin, function (req, res, next) {
   res.render('index', {
     title: 'Customers',
-    view: 'customers'
+    view: 'customers',
+    data
   });
 });
 
 router.get('/groups', userController.allowIfLoggedin, function (req, res, next) {
   res.render('index', {
     title: 'Groups',
-    view: 'groups'
+    view: 'groups',
+    data
   });
 });
-
 module.exports = router;
