de1f968fa69474e2a224f887e81d912c087060e8
cjlanwick <joshua.juanchini@gmail.com>
2019-12-26 02:20:17 -0300

    Use cards to show groups

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -5,4 +5,13 @@ function handleHeaderAction(obj, modal) {
             fadeDuration: 100
         });
     });
-}
+};
+
+$(document).ready(function(){
+    $('.card-item').mouseover(function(e){
+        $(this).children(".card-body-actions").children(".card-body-action").show();
+    });
+    $('.card-item').mouseout (function(e){
+        $(this).children(".card-body-actions").children(".card-body-action").hide();
+    });
+});
